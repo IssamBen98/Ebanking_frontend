799bcea2de77a9034b63aa731d760d807f714403
IssamBen98 <benatik.issam@gmail.com>
2022-06-26 16:14:14 +0100

    add/ delete customer

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -37,6 +37,26 @@ export class CustomersComponent implements OnInit {
         this.errorMessage=err.message;
         return throwError(err);
       })
-    );  }
+    );
+  }
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure?");
+    if(!conf) return;
+    this.customerService.deleteCustomer(c.id).subscribe({
+      next : (resp) => {
+        this.customers=this.customers.pipe(
+          map(data=>{
+            let index=data.indexOf(c);
+            data.slice(index,1)
+            return data;
+          })
+        );
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+  }
 
 }
